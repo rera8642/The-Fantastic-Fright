ab01bcd8024f1a8695f1ef9178de1ef848e2bed3
Reda Razza <94404873+rera8642@users.noreply.github.com>
2023-10-24 20:01:54 -0600

    Added Ghost Model + Interactible

--- Assets/Scripts/PlayerTeleport.cs
+++ Assets/Scripts/PlayerTeleport.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class PlayerTeleport : MonoBehaviour
 {
     [SerializeField] private LayerMask teleportLayer;
+    [SerializeField] private LayerMask ghostLayer;
     [SerializeField] private GameObject teleportVisualPrefab;
     private GameObject teleportVisual;
     private Vector3 finalPosition = Vector3.zero;
+    private bool canTeleport = true;
     
     void Update()
     {
@@ -18,7 +20,23 @@ public class PlayerTeleport : MonoBehaviour
                 Destroy(teleportVisual);
             }
             finalPosition = transform.position;
-        } if (Input.GetMouseButton(0)) {
+            canTeleport = true;
+        }
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            bool hitFound = Physics.Raycast(
+                transform.position,
+                Camera.main.transform.forward,
+                out hit,
+                Mathf.Infinity,
+                ghostLayer
+            );
+            if (hitFound) {
+                canTeleport = false;
+                Debug.Log("WOAH");
+            }
+        }
+        if (Input.GetMouseButton(0) && canTeleport) {
             RaycastHit hit;
             bool hitFound = Physics.Raycast(
                 transform.position,
